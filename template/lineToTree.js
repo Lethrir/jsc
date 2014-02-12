71786fc222fb0552421aaa716d491bf4058b09d4
Kim Richmond <kim@richmondstudios.net>
2014-02-12 16:07:49 +0000

    Fixes

--- template/lineToTree.js
+++ template/lineToTree.js
@@ -1,5 +1,7 @@
 function lineToTree(line, symbolTable){
 	var root;
+	var lastSymbol = '';
+	var lastOperator = '';
 	for(var i = 0; i<line.length; i++){
 		var token = line[i];
 		if(token.name === "var"){
@@ -11,10 +13,23 @@ function lineToTree(line, symbolTable){
 			token = line[i];
 			symbolTable.addSymbol(token);
 		}
+		if(is1LOperator(token)){ // 1 operand, to the left - eg ++ as in i++
+			// get the last symbol and use it as input to this operator
+		} else if(is1ROperator(token)){ // 1 operand, to the right - eg ++ as in ++i
+			lastOperator = token;
+		} else if(is2Operator(token)){ // 2 operands, eg + as in a + b
+			// get last symbol
+			lastOperator = token;
+		} else if(isSymbol(token))
+			// todo: check if we have an operator waiting for us
+			// otherwise, this is the last symbol
+			lastSymbol = token;
+		} else {
+			// who knows why we'd get here!
+		}
 
 	}
 }
-
 
 function symbolTable(){
 	this.symbols = [];
@@ -25,7 +40,7 @@ symbolTable.prototype.addSymbol = function(symbol) {
 	return this.symbols.length;
 };
 
-sym.prototype.findSymbol = function(symbol){
+symbolTable.prototype.findSymbol = function(symbol){
 
 };
 
